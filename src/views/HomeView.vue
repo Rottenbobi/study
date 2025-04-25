87b878cbe282aa3983c9d061797f95f7d93f1cbe
“🐱” <talon1111@163.com>
2025-04-25 09:43:32 +0800

    初始化

---
+++ src/views/HomeView.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+const goToList = (data:string) => {
+  router.push(data)
+}
+</script>
+
+<template>
+  <div class="home">
+    <h1>欢迎来到首页</h1>
+    <a-button type="primary" @click="goToList('/list')">进入列表页</a-button>
+    <a-button type="primary" @click="goToList('/threeJs')">进入threeJs</a-button>
+    <a-button type="primary" @click="goToList('/luckydraw')">进入抽奖页面</a-button>
+    <a-button type="primary" @click="goToList('/todolist')">进入todolist</a-button>
+  </div>
+</template>
+
+<style scoped>
+.home {
+  padding: 20px;
+  text-align: center;
+}
+</style>
---
+++ vite.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: './',
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+        assetFileNames: 'assets/[name].[hash].[ext]',
+        chunkFileNames: 'assets/[name].[hash].js',
+        entryFileNames: 'assets/[name].[hash].js',
+      }
+    }
+  },
+  server: {
+    host: true,
+    port: 3000
+  }
+})
